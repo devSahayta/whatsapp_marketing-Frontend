f07d416a2961cc5406253f36db8a21ab7ba0d29c
thivagarradhakrishnan-create <thivagarradhakrishnan@sahayta.co.in>
2026-04-09 16:45:13 +0530

    Embeded Signup form

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
+  plugins: [
+    react(),
+    basicSsl(), // ← add this
+  ],
+  server: {
+    https: true,
+    port: 5173,
   },
 });
